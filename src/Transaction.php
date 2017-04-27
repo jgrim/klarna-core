45c239b08975db249b638645d57758e6fbf93b8f
Jason Grim <jason.grim@klarna.com>
2017-04-27 11:12:47 -0400

    Added license and other misc cleanup

--- src/Transaction.php
+++ src/Transaction.php
@@ -1,24 +1,43 @@
 <?php namespace KlarnaCore;
 
+/**
+ * Copyright 2017 Jason Grim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    KlarnaCore
+ * @author     Jason Grim <me@jasongrim.com>
+ */
+
 use KlarnaCore\Traits\Arrayable;
 use KlarnaCore\Traits\PropertyOverload;
 
 /**
  * Class Transaction
  *
- * @property string                      $purchase_country
- * @property string                      $purchase_currency
- * @property string                      $locale
- * @property string                      $merchant_reference1
- * @property string                      $merchant_reference2
- * @property array|Address               $billing_address
- * @property array|Address               $shipping_address
- * @property array|Customer              $customer
- * @property array|Options               $options
- * @property array|MerchantUrls          $merchant_urls
- * @property array|ShippingOption        $shipping_options
- * @property array|ExternalCheckouts     $external_checkouts
- * @property array|ExternalPaymentMethod $external_payment_methods
+ * @property string              $purchase_country
+ * @property string              $purchase_currency
+ * @property string              $locale
+ * @property string              $merchant_reference1
+ * @property string              $merchant_reference2
+ * @property array|Address       $billing_address
+ * @property array|Address       $shipping_address
+ * @property array|Customer      $customer
+ * @property array|Options       $options
+ * @property array|MerchantUrls  $merchant_urls
+ * @property array|ModelAbstract $shipping_options
+ * @property array|ModelAbstract $external_checkouts
+ * @property array|ModelAbstract $external_payment_methods
  *
  * @package KlarnaCore
  */
